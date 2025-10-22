d3d29569c10987d44f0e1ddd12e3bf85b247e538
IvanSabljić <kalarhont@gmail.com>
2025-10-22 18:45:02 +0200

    Prvi commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+    }
+}
